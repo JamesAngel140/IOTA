2f7988f1690c2e9f5b376831644bea842666d59e
james <jamesangel140@gmail.com>
2016-10-29 21:19:37 +0100

    setting up login screen

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.use(express.static(__dirname + '/public'));
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });

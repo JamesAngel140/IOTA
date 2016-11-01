7afc41f10ac993976e00d90cba122f58c2879cce
James Angel <jamesangel140@gmail.com>
2016-11-01 20:22:43 +0000

    Login page withajax call not working

--- app.js
+++ app.js
@@ -2,12 +2,27 @@
  * Created by james on 29/10/2016.
  */
 
+// $.ajax({
+//     type: "POST",
+//     url: http://localhost:3000/action_page.php?uname=James,
+//     data: uname,
+//     success:,
+//     dataType: String
+// });
+
 var express = require('express');
 var app = express();
 var path = require('path');
 
+var bodyParser = require('body-parser');
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post('/register', function(req, res) {
+    console.log("register");
+    console.log(req);
 });
 
 app.use(express.static(__dirname + '/public'));

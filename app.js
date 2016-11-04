3e2f2429701844c8626a0edf9f6f2f2b9d3e4156
james <jamesangel140@gmail.com>
2016-11-04 14:15:59 +0000

    setting up login screen

--- app.js
+++ app.js
@@ -14,15 +14,30 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var registeredUsers = [];
+
 var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.post('/register', function(req, res) {
-    console.log("register");
-    console.log(req);
+    console.log('body: ' + JSON.stringify(req.body));
+
+    if(typeof req.body === 'undefined' || typeof req.body.name === 'undefined'){
+        res.sendStatus(400);
+    } else {
+        registeredUsers.push(req.body.name);
+        res.sendStatus(200);
+    }
+    console.log(registeredUsers);
+});
+
+app.get('/users', function(req, res) {
+    var message = JSON.stringify(registeredUsers);
+    res.send(registeredUsers);
 });
 
 app.use(express.static(__dirname + '/public'));

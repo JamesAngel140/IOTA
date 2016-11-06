e2d6bbd773edf8830c0caaca3ab9a4094bb3d95f
James Angel <jamesangel140@gmail.com>
2016-11-06 21:57:44 +0000

    Login page working

--- app.js
+++ app.js
@@ -2,31 +2,26 @@
  * Created by james on 29/10/2016.
  */
 
-// $.ajax({
-//     type: "POST",
-//     url: http://localhost:3000/action_page.php?uname=James,
-//     data: uname,
-//     success:,
-//     dataType: String
-// });
+var express = require('express'),
+    app = express(),
+    http = require("http").createServer(app),
+    bodyParser = require("body-parser"),
+    path = require('path');
 
-var express = require('express');
-var app = express();
-var path = require('path');
-
+// global variable of users
 var registeredUsers = [];
 
-var bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(express.static('node_modules'));
+app.use(express.static('public'));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-
-app.post('/register', function(req, res) {
+app.post('/register', function (req, res) {
     console.log('body: ' + JSON.stringify(req.body));
 
-    if(typeof req.body === 'undefined' || typeof req.body.name === 'undefined'){
+    if (typeof req.body === 'undefined' || typeof req.body.name === 'undefined') {
         res.sendStatus(400);
     } else {
         registeredUsers.push(req.body.name);
@@ -34,14 +29,10 @@ app.post('/register', function(req, res) {
     }
     console.log(registeredUsers);
 });
-
-app.get('/users', function(req, res) {
+app.get('/users', function (req, res) {
     var message = JSON.stringify(registeredUsers);
     res.send(registeredUsers);
 });
-
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/node_modules'));
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
---
+++ public/js/application.js
@@ -0,0 +1,59 @@
+/**
+ * Created by James on 06/11/2016.
+ */
+$(document).ready(function () {
+
+    function login_success(result, status, xhr) {
+        alert("login success");
+
+        $("#loginbox").hide();
+        $("#header").show();
+
+        // every 5000 milliseconds (5 seconds) get the registered users from the server
+        setInterval(function(){
+            $.ajax({
+                type: 'GET',
+                url: '/users',
+                success: function (data) {
+                    console.log('success');
+                    console.log(JSON.stringify(data));
+                    data.forEach(function(user,index){
+                        $("#user" + index).html(user);
+                    });
+                }
+            });
+        }, 5000);
+    }
+
+    function login_error(xhr, status, error) {
+        alert("login error " + error);
+    }
+
+    function login() {
+        var data = {name: $("#login-username").val()};
+
+        $.ajax({
+            type: 'POST',
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            url: '/register',
+            success: login_success
+            // success: function (data) {
+            //     console.log('success');
+            //     console.log(JSON.stringify(data));
+            //     $("#loginbox").hide();
+            //     $("#header").show();
+            // }
+        });
+    }
+
+    $("#btn-login").click(function () {
+        login();
+    });
+
+    $("#loginform").submit(function (event) {
+        event.preventDefault();
+        login();
+    });
+
+});

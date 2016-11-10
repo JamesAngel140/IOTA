54f49d6b937b16dcb9aba8a1e923b4877be09049
james <jamesangel140@gmail.com>
2016-11-10 20:55:36 +0000

    tiles can be selected and added to the grid

--- public/js/application.js
+++ public/js/application.js
@@ -10,14 +10,14 @@ $(document).ready(function () {
         $("#users").show();
 
         // every 5000 milliseconds (5 seconds) get the registered users from the server
-        setInterval(function(){
+        setInterval(function () {
             $.ajax({
                 type: 'GET',
                 url: '/users',
                 success: function (data) {
                     console.log('success');
                     console.log(JSON.stringify(data));
-                    data.forEach(function(user,index){
+                    data.forEach(function (user, index) {
                         $("#user" + index).html(user);
                     });
                 }
@@ -35,20 +35,21 @@ $(document).ready(function () {
     // });
     // $("#tiles").html(html.join("\n"));
 
-    function gridToHtml(grid)
-    {
+    function gridToHtml(grid) {
         html = '<table>';
-        for(var y = 0; y < grid.length; y++){
+        for (var y = 0; y < grid.length; y++) {
             html += '<tr>';
-            for(var x = 0; x < grid[y].length; x++){
-                if(grid[x][y] !== null){
-                    switch(grid[x][y].type){
+            for (var x = 0; x < grid[y].length; x++) {
+                if (grid[x][y] !== null) {
+                    switch (grid[x][y].type) {
                         case "tile":
-                            html += '<td>' + tileToHtml(grid[x][y]) + '</td>';
+                            html += '<td>' + tileToHtml(grid[x][y], x, y) + '</td>';
                             break;
                         case "target":
-                            html += '<td>' + targetToHtml(grid[x][y],x,y) + '</td>';
+                            html += '<td>' + targetToHtml(grid[x][y], x, y) + '</td>';
                             break;
+                        case "blank":
+                            html += '<td>' + blankToHtml(grid[x][y]) + '</td>';
                     }
 
                 } else {
@@ -61,13 +62,35 @@ $(document).ready(function () {
         return html;
     }
 
-    function targetToHtml(tile, x, y){
+    function handToHtml(hand) {
+        html = '<table>';
+        html += '<tr>';
+        for (var y = 0; y < hand.length; y++) {
+            switch (hand[y].type) {
+                case "tile":
+                    html += '<td>' + tileToHtml(hand[y], -1, y) + '</td>';
+                    break;
+                case "blank":
+                    html += '<td>' + blankToHtml(hand[y]) + '</td>';
+            }
+        }
+        html += '</tr>';
+        html += '</table>';
+        return html;
+    }
+
+    function targetToHtml(tile, x, y) {
         var html = '<div class="target" data-x="' + x + '" data-y="' + y + '"></div>';
         return html;
     }
 
-    function tileToHtml(tile){
-        var html = '<div class="tileback"><span class="alignleft">'+
+    function blankToHtml(tile) {
+        var html = '<div class="blank"></div>';
+        return html;
+    }
+
+    function tileToHtml(tile, x, y) {
+        var html = '<div id="x' + x + 'y' + y + '" class="tile" data-x="' + x + '" data-y="' + y + '"><span class="alignleft">' +
             tile.number +
             '</span><span class="alignright">' +
             tile.number +
@@ -83,26 +106,52 @@ $(document).ready(function () {
         return html;
     }
 
-    function addTargets(grid, x, y){
-        for(var a = x-1; a <= x+1; a++){
-            for(var b = y-1; b <= y+1; b++) {
-                if(grid[a][b] === null){
-                    grid[a][b] = {type:"target"};
+    function addTargets(grid, x, y) {
+        for (var a = x - 1; a <= x + 1; a++) {
+            for (var b = y - 1; b <= y + 1; b++) {
+                if (grid[a][b] === null || grid[a][b].type === "blank") {
+                    grid[a][b] = {type: "target"};
                 }
             }
         }
     }
 
-    function addTargetsToGrid(grid){
-        for(var x = 0; x < grid.length; x++){
-            html += '<tr>';
-            for(var y in grid[x]){
-                if(grid[x][y] !== null){
-                    switch(grid[x][y].type){
+    function addBlanks(grid, x, y) {
+        var size = 3;
+        for (var a = x - size; a <= x + size; a++) {
+            for (var b = y - size; b <= y + size; b++) {
+                if (grid[a][b] === null) {
+                    grid[a][b] = {type: "blank"};
+                }
+            }
+        }
+    }
+
+    function addTargetsToGrid(grid) {
+        for (var x = 0; x < grid.length; x++) {
+            for (var y = 0; y < grid[x].length; y++) {
+                if (grid[x][y] !== null) {
+                    switch (grid[x][y].type) {
                         case "tile":
-                            addTargets(grid,x,y);
+                            addTargets(grid, x, y);
+                            //addBlanks(grid, x, y);
                             break;
                         case "target":
+                            //addBlanks(grid, x, y);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    function addBlanksToGrid(grid) {
+        for (var x = 0; x < grid.length; x++) {
+            for (var y = 0; y < grid[x].length; y++) {
+                if (grid[x][y] !== null) {
+                    switch (grid[x][y].type) {
+                        case "target":
+                            addBlanks(grid, x, y);
                             break;
                     }
                 }
@@ -111,28 +160,67 @@ $(document).ready(function () {
     }
 
     // grid[32][32] = {type:"target"};
-    grid[32][32] = deck[4];
-    grid[32][33] = deck[17];
-    grid[31][32] = deck[50];
+    // grid[32][32] = deck[4];
+    // grid[32][33] = deck[17];
+    // grid[31][32] = deck[50];
+
+    grid[32][32] = {type: "target"};
 
     addTargetsToGrid(grid);
+    addBlanksToGrid(grid);
 
-    var html = gridToHtml(grid);
+    dealHand();
+    var html = handToHtml(hand);
+    $("#hand").html(html);
+
+    html = gridToHtml(grid);
     $("#grid").html(html);
 
-    function targetClick(e){
-            var x = $(this).attr("data-x");
-            var y = $(this).attr("data-y");
-            console.log(x,y);
-            // $(this).addClass("targetSelected");
-            grid[x][y] = deck[5];
+    function targetClick(e) {
+        var x = $(this).attr("data-x");
+        var y = $(this).attr("data-y");
+        console.log(x, y);
+        $(this).addClass("targetSelected");
+        // grid[x][y] = deck[5];
+
+        // swap item out of hand into grid
+        if(handIndex === -1){
+            return;
+        }
+
+        grid[x][y] = hand[handIndex];
+
+        hand[handIndex] = {type: "blank"};
+        var html = handToHtml(hand);
+        $("#hand").html(html);
+        handIndex = -1;
+        $('#hand .tile').click(tileClick);
+
         addTargetsToGrid(grid);
-        var html = gridToHtml(grid);
-            $("#grid").html(html);
+        html = gridToHtml(grid);
+        $("#grid").html(html);
         $('.target').click(targetClick);
     }
 
     $('.target').click(targetClick);
+
+    var handIndex = -1;
+    function tileClick(e) {
+        if(handIndex !== -1){
+            $('#x-1y' + handIndex).removeClass("targetSelected");
+        }
+        // var x = $(this).attr("data-x");
+        var y = $(this).attr("data-y");
+        handIndex = y;
+        console.log(x, y);
+        $(this).addClass("targetSelected");
+        // grid[x][y] = deck[5];
+        // addTargetsToGrid(grid);
+        // var html = gridToHtml(grid);
+        // $("#grid").html(html);
+        // $('.target').click(targetClick);
+    }
+    $('#hand .tile').click(tileClick);
 
     function login_error(xhr, status, error) {
         alert("login error " + error);
--- public/js/tile.js
+++ public/js/tile.js
@@ -2,11 +2,13 @@
  * Created by James on 07/11/2016.
  */
 
-var  shapes=["square", "circle", "triangle", "cross"],
+// Create the 3 variables of a tile
+var shapes=["square", "circle", "triangle", "cross"],
     colours=["blue", "red", "green", "yellow"],
     numbers=["1", "2", "3", "4"],
     deck=[];
 
+// using the variables above create a deck of cards
 shapes.forEach(function(shape){
     colours.forEach(function(colour) {
         numbers.forEach(function (number) {
@@ -15,4 +17,35 @@ shapes.forEach(function(shape){
     })
 });
 
-console.log(deck);
+// The Fisher-Yates (aka Knuth) Shuffle.
+// See https://github.com/coolaj86/knuth-shuffle
+// You can see a great visualization here (and the original post linked to this)
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+// shuffle the deck so that it has a random order
+deck = shuffle(deck);
+//console.log(deck);
+
+var hand = [];
+
+function dealHand(){
+    for(var i = 0; i < 4; i++){
+        hand.push(deck.pop());
+    }
+}
